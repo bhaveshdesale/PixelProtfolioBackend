80e938d4c18fe15816a19d97534b49e348761226
Bhavesh <143746617+bhaveshdesale@users.noreply.github.com>
2025-12-31 14:59:43 +0530

    final commit

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import visitorRoutes from "./routes/visitor.routes";
-import contactRoutes from "./routes/contact.routes"
+import contactRoutes from "./routes/contact.routes";
 
 const app = express();
 
-// app.use(cors({ origin: "https://pixel-protfolio-frontend-x8sd.vercel.app/" })); // Vite frontend
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://pixel-protfolio-frontend-x8sd.vercel.app/" // 👈 ADD THIS
+      "https://pixel-protfolio-frontend-x8sd.vercel.app"
     ],
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
   })
 );
+
+// IMPORTANT
+app.options("*", cors());
+
 app.use(express.json());
 
 app.use("/api/visitors", visitorRoutes);
