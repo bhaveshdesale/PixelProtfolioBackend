24dce297822915ee2522288ba276a75e6fa705ec
Bhavesh <143746617+bhaveshdesale@users.noreply.github.com>
2025-12-31 05:43:21 +0530

    finally

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,16 @@ import contactRoutes from "./routes/contact.routes"
 
 const app = express();
 
-app.use(cors({ origin: "https://pixel-protfolio-frontend-x8sd.vercel.app/" })); // Vite frontend
+// app.use(cors({ origin: "https://pixel-protfolio-frontend-x8sd.vercel.app/" })); // Vite frontend
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://pixel-protfolio-frontend-x8sd.vercel.app/" // 👈 ADD THIS
+    ],
+    methods: ["GET", "POST"],
+  })
+);
 app.use(express.json());
 
 app.use("/api/visitors", visitorRoutes);
